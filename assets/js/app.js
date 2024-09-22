65a377c61f6d0e9af91cdff235e382d002d6db0a
devluizmarcolino <devluizmarcolino@gmail.com>
2024-09-22 00:42:01 -0300

    :star: header concluído

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header-container p", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header-container h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
